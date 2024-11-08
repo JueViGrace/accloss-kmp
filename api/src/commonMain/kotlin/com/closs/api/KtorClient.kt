393cf00f1729a7b808d83436a36fc36d464c555d
Daniel Zabala <danielzabalanava@gmail.com>
2024-11-08 15:37:53 -0400

    network module added

---
+++ api/src/commonMain/kotlin/com/closs/api/KtorClient.kt
@@ -0,0 +1,42 @@
+package com.closs.api
+
+import com.closs.types.common.Constants.BASE_URL
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.SIMPLE
+import io.ktor.http.HttpHeaders
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object KtorClient {
+    fun client(baseUrl: String? = null): HttpClient {
+        return HttpClient(CIO) {
+            install(Logging) {
+                logger = Logger.SIMPLE
+                level = LogLevel.BODY
+
+                sanitizeHeader { header -> header == HttpHeaders.Authorization }
+            }
+
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        prettyPrint = true
+                        ignoreUnknownKeys = true
+                        encodeDefaults = true
+                        explicitNulls = true
+                    }
+                )
+            }
+
+            install(DefaultRequest) {
+                url(baseUrl ?: BASE_URL)
+            }
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -32,4 +32,5 @@ include(
     ":composeApp",
     ":database",
     ":types",
+    ":api",
 )
